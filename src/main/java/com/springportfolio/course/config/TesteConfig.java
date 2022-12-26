20593aafa75cc0ee2ca218819b546c782fa00b89
Luiz Felipe <luiztrindade01@outlook.com>
2022-12-26 17:00:25 -0300

    created order itens and product-orderitem one-to-many association

--- src/main/java/com/springportfolio/course/config/TesteConfig.java
+++ src/main/java/com/springportfolio/course/config/TesteConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.springportfolio.course.entities.Category;
 import com.springportfolio.course.entities.Order;
+import com.springportfolio.course.entities.OrderItem;
 import com.springportfolio.course.entities.Product;
 import com.springportfolio.course.entities.User;
 import com.springportfolio.course.enums.OrderStatus;
 import com.springportfolio.course.repositories.CategoryRepo;
+import com.springportfolio.course.repositories.OrderItemRepo;
 import com.springportfolio.course.repositories.OrderRepo;
 import com.springportfolio.course.repositories.ProductRepo;
 import com.springportfolio.course.repositories.UserRepo;
@@ -35,6 +37,10 @@ public class TesteConfig implements CommandLineRunner {
 	@Autowired
 	private ProductRepo productRepository;
 	
+	@Autowired
+	private OrderItemRepo orderitemrepository;
+	
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -42,12 +48,29 @@ public class TesteConfig implements CommandLineRunner {
 		Category cat2 = new Category(null, "Books"); 
 		Category cat3 = new Category(null, "Computers"); 
 		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
+		
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "", null);
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "", null);
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "", null);
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "", null);
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "", null); 
-
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		
+		
 		User u1 = new User("Maria Brown", "maria@gmail.com", "988888888", null, "123456");
 		User u2 = new User("Alex Green", "alex@gmail.com", "97777777", null, "123456");
 
@@ -57,8 +80,17 @@ public class TesteConfig implements CommandLineRunner {
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		orderitemrepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		
+		
+		
 	}
 
 }
---
+++ src/main/java/com/springportfolio/course/repositories/OrderItemRepo.java
@@ -0,0 +1,9 @@
+package com.springportfolio.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springportfolio.course.entities.OrderItem;
+
+public interface OrderItemRepo extends JpaRepository<OrderItem, Long> {
+
+}
