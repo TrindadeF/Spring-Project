0298850a0a9668f4643cef8792febc32de2fce43
Luiz Felipe <luiztrindade01@outlook.com>
2023-01-16 17:57:55 -0300

    Cors addeded to project

--- src/main/java/com/springportfolio/course/config/TesteConfig.java
+++ src/main/java/com/springportfolio/course/config/TesteConfig.java
@@ -74,12 +74,13 @@ public class TesteConfig implements CommandLineRunner {
 		
 		User u1 = new User("Maria Brown", "maria@gmail.com", "988888888", null, "123456");
 		User u2 = new User("Alex Green", "alex@gmail.com", "97777777", null, "123456");
+		User u3 = new User("Luiz Felipe", "felipe@gmail.com", "98184184", null, "123456");
 
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.Paid, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.Paid, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.DELIVERED, u1);
 
-		userRepository.saveAll(Arrays.asList(u1, u2));
+		userRepository.saveAll(Arrays.asList(u1, u2, u3));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
 		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
